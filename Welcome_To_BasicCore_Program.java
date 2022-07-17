9c3bb05c1d3f76603b9525ca40b0cb40d8289982
biradarajay1904 <biradarajay04@gmail.com>
2022-07-17 16:50:40 +0530

    modified: factors of number

--- Welcome_To_BasicCore_Program.java
+++ Welcome_To_BasicCore_Program.java
@@ -1,27 +1,23 @@
 package com.Basic.Program;
 import java.util.Scanner;
 public class Welcome_To_BasicCore_Program {
+	static int n;
 	public static void main(String[] args) {
 
 		
-		int h_cnt = 0; int t_cnt = 0;
-		Scanner Scan = new Scanner(System.in);
-		System.out.println("Enter the no of times to flip a coin:");
-		int n = Scan.nextInt();
-		for (int i = 1; i < n; i++) {
-			double flip = Math.floor(Math.random() * 10) % 2;
-			if (flip< 0.5) {
-				h_cnt++;
-			} else {
-				t_cnt++;
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the any number:");
+	      n = sc.nextInt();
+	      System.out.print("The Prime Factors of " + n + " are : ");
+
+			int i = 2;
+			while (n > 1) {
+				if (n % i == 0) {
+					System.out.print(i + " ");
+					n = n / i;
+				} else
+					i++;
 			}
-			
-		}
-		double headpercentage = (h_cnt* 100) /n;
-		double tailpercentage = (t_cnt * 100) /n;
-		System.out.println("The percentage of head flips:" + headpercentage);
-		System.out.println("The percentage of tail flips:" + tailpercentage);
-		
-		
+			System.out.println(" ");
 	}
-}
+		}
