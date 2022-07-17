6f500852d39dd14ac91fbe0d0a78fb55ab3282b3
biradarajay1904 <biradarajay04@gmail.com>
2022-07-17 15:30:28 +0530

    welcome to basic core program

---
+++ Welcome_To_BasicCore_Program.java
@@ -0,0 +1,10 @@
+package com.Basic.Program;
+
+public class Welcome_To_BasicCore_Program {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Basic Core program");
+
+	}
+
+}
