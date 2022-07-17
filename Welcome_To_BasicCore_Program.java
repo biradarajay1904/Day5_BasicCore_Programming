5f56690c48165e42e25047c18ffe6f1fc740e6a9
biradarajay1904 <biradarajay04@gmail.com>
2022-07-17 16:58:25 +0530

    modified: even or odd number

--- Welcome_To_BasicCore_Program.java
+++ Welcome_To_BasicCore_Program.java
@@ -1,25 +1,18 @@
 package com.Basic.Program;
-
+import java.util.Scanner;
 public class Welcome_To_BasicCore_Program {
-	static int first= 12;  static int second = 14;
 	public static void main(String[] args) {
-		System.out.println("--Before swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
+	Scanner reader = new Scanner(System.in);
 
-        // Value of first is assigned to temporary
-        int  temporary = first;
-
-        // Value of second is assigned to first
-        first = second;
-
-        // Value of temporary (which contains the initial value of first) is assigned to second
-        second = temporary;
-
-        System.out.println("--After swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
+	  System.out.print("Enter a number: ");
+	  
+	  int num = reader.nextInt();
+	  if(num % 2 == 0)
+		  System.out.println(num + " is even");
+	  else
+			            System.out.println(num + " is odd");
 		
-	}
 
-}
+		}
+
+	}
